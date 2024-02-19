60f8d238e9b170a8028777576710163933fe733f
gleidson fagno <gleidsonfagner838@gmail.com>
2024-02-19 01:24:11 +0000

    modificando pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home() {
-    return <h1>Page Home</h1>
+import { FaGithub } from 'react-icons/fa';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Code News</h1>
+            <p>A tecnologia molda o futuro. Explore as últimas novidades e descubra como a inovação está transformando o mundo ao seu redor!</p>
+            <a href="https://github.com/gleidsonfagno/CodeVoyage" target="_blank" rel="noopener noreferrer">
+                <FaGithub /> Confira meu GitHub
+            </a>
+        </div>
+    );
 }
 
-export default Home;
