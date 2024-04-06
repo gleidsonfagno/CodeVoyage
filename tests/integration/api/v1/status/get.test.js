61203d1ce09745e5c63e96534453a94826b2f6e2
gleidson fagno <gleidsonfagner838@gmail.com>
2024-04-06 13:46:13 +0000

    :arrows_counterclockwise: Atualizando arquivo [tests/integration/]

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,8 @@
+const { SiExpertsexchange } = require("react-icons/si");
+
+test("GET to /api/v1/status shold return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  expect(response.status).toBe(200);
+  // console.log(response);
+});
